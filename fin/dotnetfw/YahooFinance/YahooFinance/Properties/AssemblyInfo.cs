6133ad0b8c786b6db9a0445087acca958e7ebe82
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-04-10 11:56:28 -0400

    Implemented new folder structure for .netcore and .netfw

---
+++ fin/dotnetfw/YahooFinance/YahooFinance/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("yfinancedemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("yfinancedemo")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1dd890c-e3a3-404c-97d1-4bee5e35bc45")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ fin/dotnetfw/YahooFinance/YahooFinance/TestGetData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using YahooFinanceApi;
+
+namespace yfinancedemo
+{
+    [TestClass]
+    public class TestGetData
+    {
+        // string[] faang = { "FB", "AMZN", "AAPL", "NFLX", "GOOG" };
+        string faang = "msft";
+
+        [TestMethod]
+        public async Task TestGetDataAsync()
+        {
+            IReadOnlyDictionary<string, Security> securities = 
+                await Yahoo.Symbols(faang).Fields(Field.Symbol, Field.RegularMarketPrice, Field.FiftyTwoWeekHigh).QueryAsync();
+            
+            var msft = securities["MSFT"];
+
+            var symbol = msft[Field.Symbol];
+            
+            var price = msft[Field.RegularMarketPrice];
+            
+            Console.WriteLine($"{symbol}: {price}");
+        }
+
+        [TestMethod]
+        public async Task TestGetDataAsync2()
+        {
+            IReadOnlyDictionary<string, Security> securities = 
+                await Yahoo.Symbols(faang).Fields("Symbol", "RegularMarketPrice").QueryAsync();
+
+            // Console.WriteLine($"{securities["AAPL"].Symbol}\n{securities["AAPL"].RegularMarketPrice}");
+
+            foreach (var security in securities)
+            {
+                Console.WriteLine($"{security.Key}, {security.Value.RegularMarketPrice}");
+            }
+        }
+    }
+}
