cfa3c385bf05be0fb988db2116c444e335d6d82b
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-04-11 17:09:19 -0400

    Added Secure Store class

---
+++ fin/dotnetcore/TradierClient/UnitTestProject/Helpers/SecureStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace UnitTestProject.Helpers
+{
+    class SecureStore
+    {
+        private static readonly string creds = @"C:\SecureStore\token_endpoint.txt";
+
+        /// <summary>
+        /// Read Endpoint Uri and Access Token from file in Secure Store.
+        /// </summary>
+        /// <returns></returns>
+        internal static string GetCreds()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(creds))
+                {
+                    return sr.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
--- fin/dotnetcore/TradierClient/UnitTestProject/UnitTest.cs
+++ fin/dotnetcore/TradierClient/UnitTestProject/UnitTest.cs
@@ -2,23 +2,65 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using UnitTestProject.Helpers;
+using System.Text.RegularExpressions;
 
 namespace UnitTestProject
 {
     [TestClass]
     public class UnitTest
     {
-        private string getUri = "https://sandbox.tradier.com/v1/markets/quotes?symbols=";
-        private string accessToken = "";
+        private readonly string[] creds = SecureStore.GetCreds().Split(',');
 
-        public string TestGetData(string symbol)
+        private string accessToken = string.Empty;
+        public string AccessToken
         {
+            get 
+            {
+                return accessToken;
+            }
+            set 
+            {
+                Assert.IsTrue(Regex.IsMatch(creds[0], "^[a-zA-Z0-9]*$"));
+
+                if (Regex.IsMatch(creds[0], "^[a-zA-Z0-9]*$"))
+                {
+                    accessToken = creds[0];
+                }
+            }
+        }
+        
+        private string uri = string.Empty;
+        public string Uri
+        {
+            get 
+            {
+                return uri;
+            }
+            set 
+            {
+                Assert.IsTrue(Regex.IsMatch(creds[1], "^https?://"));
+                // Assert.IsTrue(Regex.IsMatch(creds[1], "^(https|https)://"));
+
+                if (Regex.IsMatch(creds[1], "^https?://"))
+                {
+                    uri = creds[1];
+                }
+            }
+        }
+
+        public string TestGetQuoteAsJson(string symbol)
+        {
+            Assert.AreEqual(2, creds.Length);
+            AccessToken = creds[0];
+            Uri = creds[1];
+
             using (HttpClient httpClient = new HttpClient())
             {
                 httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
-                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {AccessToken}");
 
-                Task<HttpResponseMessage> httpResponseMessage = httpClient.GetAsync($"{getUri}{symbol}");
+                Task<HttpResponseMessage> httpResponseMessage = httpClient.GetAsync($"{Uri}{symbol}");
 
                 Assert.AreEqual(200, (int)httpResponseMessage.Result.StatusCode);
 
@@ -32,9 +74,9 @@ namespace UnitTestProject
         }
 
         [TestMethod]
-        public void TestClient()
+        public void TestGetQuote()
         {
-            Console.WriteLine(TestGetData("msft"));
+            Console.WriteLine(TestGetQuoteAsJson("msft"));
         }
     }
 }
