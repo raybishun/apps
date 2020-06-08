13eb93544a27f2c251e77fbedd2bf4c5de94f3b8
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-06-07 22:33:03 -0400

    Added MDATP PoC UnitTest Project

---
+++ it/dotnetfw/UnitTestMDATP/UnitTestMDATP/MDATPPoCUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace UnitTestMDATP
+{
+    [TestClass]
+    public class MDATPPoCUnitTest
+    {
+        [TestMethod]
+        public void TestMethodHelloWorld()
+        {
+            // Use of a hosted REST-API ready to respond to your AJAX requests at: https://reqres.in/
+            var client = new RestClient("https://reqres.in/");
+            var request = new RestRequest("/api/users/2", DataFormat.Json);
+            var response = client.Get(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
