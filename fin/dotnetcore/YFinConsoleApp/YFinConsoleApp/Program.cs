9b5dbf645e0ffaa32d7d6ae2049a6685ce0e9ef0
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-09-30 21:53:46 -0400

    Maintenance

---
+++ fin/dotnetcore/YFinConsoleApp/YFinConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YahooFinanceApi;
+
+namespace YFinConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // discard '_'
+            _ = GetData();
+            
+            Console.ReadKey();
+        }
+
+        static async Task GetData()
+        {
+            string[] faangm = { "FB", "AMZN", "AAPL", "NFLX", "GOOG", "MSFT" };
+
+            IReadOnlyDictionary<string, Security> securities = 
+                await Yahoo.Symbols(faangm).Fields(Field.Symbol, Field.RegularMarketPrice).QueryAsync();
+
+            foreach (var security in securities)
+            {
+                Console.WriteLine($"{security.Key}, {security.Value.RegularMarketPrice}");
+            }
+        }
+    }
+}
