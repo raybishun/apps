ce691c180b2c7afcfaa191b2ab0b8e37ad07b282
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-10-12 23:59:43 -0400

    Added TestMethodGetAlertsUsingHttpClient()

--- it/dotnetcore/UnitTestMDATP/UnitTestMDATP/UnitTest1.cs
+++ it/dotnetcore/UnitTestMDATP/UnitTestMDATP/UnitTest1.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UnitTestMDATP.Utilities;
 
@@ -12,6 +15,24 @@ namespace UnitTestMDATP
         {
             string token = Auth.GetToken();
             Console.WriteLine(token);
+        }
+
+        [TestMethod]
+        public async Task TestMethodGetAlertsUsingHttpClient()
+        {
+            HttpClient client = new HttpClient();
+
+            string alertsUrl = "https://api.securitycenter.windows.com/api/alerts";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, alertsUrl);
+            
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Auth.GetToken());
+
+            HttpResponseMessage response = client.SendAsync(request).GetAwaiter().GetResult();
+
+            string alerts = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(alerts);
         }
     }
 }
