1e36f67ac84d2de75c8748235c43dd771c882993
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-10-12 22:32:16 -0400

    Added SecureStoreReader class

--- it/dotnetcore/UnitTestMDATP/UnitTestMDATP/UnitTest1.cs
+++ it/dotnetcore/UnitTestMDATP/UnitTestMDATP/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestMDATP.Utilities;
 
 namespace UnitTestMDATP
 {
---
+++ it/dotnetcore/UnitTestMDATP/UnitTestMDATP/Utilities/SecureStoreReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace UnitTestMDATP.Utilities
+{
+    class SecureStoreReader
+    {
+        public static string GetCredentials(string path)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    return sr.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
