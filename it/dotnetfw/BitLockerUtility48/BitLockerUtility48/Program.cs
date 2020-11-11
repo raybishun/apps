39a90c19436fe7ed70aeb17b4073a978cdb7dcca
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-11-10 19:20:58 -0500

    Added BitLockerUtility48

---
+++ it/dotnetfw/BitLockerUtility48/BitLockerUtility48/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Management;
+
+namespace BitLockerUtility48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetWinOSVer();
+            Console.ReadKey();
+        }
+
+        static void GetWinOSVer()
+        {
+            try
+            {
+                ConnectionOptions options = new ConnectionOptions();
+                options.Impersonation = ImpersonationLevel.Impersonate;
+                ManagementScope scope = new ManagementScope(@"\\.\root\cimv2", options);
+                scope.Connect();
+
+                // ObjectQuery query = new ObjectQuery("SELECT Caption FROM Win32_OperatingSystem");
+                ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+                ManagementObjectCollection objectCollection = searcher.Get();
+
+                foreach (var item in objectCollection)
+                {
+                    Console.WriteLine(item["Caption"].ToString());
+                    
+                    foreach (var subItem in item.Properties)
+                    {
+                        Console.WriteLine($"{subItem.Name}\t{subItem.Value}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
